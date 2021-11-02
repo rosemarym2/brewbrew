80a87f47d42d525d97d035406c3a28048647c2bc
Fluffy-Lion <leoncooper31@gmail.com>
2021-11-02 14:49:04 +0000

    added modal and basic show hide functionality

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,4 +1,25 @@
-const Modal = () => {
-    return <h2>modal</h2>
+import { useState } from "react"
+
+const Modal = (props) => {
+    const [show, setShow] = useState(false)
+    const handler = () => {
+        setShow(!show)
+    }
+    if(!props){
+        return <p>loading...</p>
+    }
+    return (
+        <div onClick={() => setShow(!show)}>
+            {show ? 
+            <div>
+                {props.children}
+                <p onClick={handler}>close</p>
+                </div> : 
+            <div>
+               {props.tips ? <p>see tips</p> : <p>see pairing</p>}
+            </div>
+            }
+        </div>
+    )
 }
 export default Modal
--- src/components/MoreInfo/MoreInfo.js
+++ src/components/MoreInfo/MoreInfo.js
@@ -1,12 +1,24 @@
-import Modal from "../Modal/Modal"
+import Modal from "../Modal/Modal";
 
+const MoreInfo = (props) => {
+  if (!props.pairing) {
+    return <p>loading...</p>;
+  }
+  return (
+    <div>
+      <h1>More Info</h1>
+      <Modal tips>
+          <h3>brewing tips</h3>
+            <p>{props.tips}</p>
+      </Modal>
+      <Modal pairing>
+        <h3>food pairing</h3>
+        {props.pairing.map((pair, index) => {
+          return <p key={index}>{pair}</p>;
+        })}
+      </Modal>
 
-const MoreInfo = () => {
-    return (
-        <div>
-            <h1>More Info</h1>
-            <Modal />
-        </div>
-    )
-}
-export default MoreInfo
+    </div>
+  );
+};
+export default MoreInfo;
